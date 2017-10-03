e634b8be8f8a9e0a16b4c7257bd307ca791fcdad
Sergei Dolgov <dolgovs@gmail.com>
2017-10-03 18:15:36 +0000

    Make an HTTP request

--- livedoc-ui-purescript/src/Main.purs
+++ livedoc-ui-purescript/src/Main.purs
@@ -1,34 +1,51 @@
 module Main where
 
 import App (view)
+import Control.Monad.Aff (attempt)
 import Control.Monad.Aff.Console (log)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE)
-import Control.Monad.Eff.Exception (EXCEPTION)
+import Control.Monad.Eff.Exception (EXCEPTION, Error)
 import DOM (DOM)
 import DOM.Event.Event (preventDefault)
+import Data.Either (Either)
 import Data.Maybe (Maybe(..))
+import Network.HTTP.Affjax (AJAX, get)
 import Prelude hiding (div)
 import Pux (EffModel, start)
 import Pux.Renderer.React (renderToDOM)
 import Signal.Channel (CHANNEL)
 import State (Event(..), State)
 
-foldp :: forall fx. Event -> State -> EffModel State Event (console :: CONSOLE, dom :: DOM | fx)
+defaultUrl :: String
+defaultUrl = "http://localhost:2002/static/jsondoc.json"
+
+foldp :: forall fx. Event -> State -> EffModel State Event (console :: CONSOLE, dom :: DOM, ajax :: AJAX | fx)
 foldp (FormUpdate s) _ =
   { state: { form: s }
   , effects: [ log ("increment " <> s) *> pure Nothing ]
   }
-foldp (FormSubmit ev) _ =
+foldp (FormSubmit ev) s =
   { state: { form: "" }
-  , effects: [ liftEff (preventDefault ev) *> log "submit" *> pure Nothing ]
+  , effects: [ liftEff (preventDefault ev) *> log "submit" *> (pure $ Just $ RequestDoc s.form) ]
+  }
+foldp (RequestDoc url) s =
+  { state: s
+  , effects: [ do
+    res <- attempt $ get url
+    log $ "received " <> show ((_.response <$> res) :: Either Error String)
+    pure $ Just $ ReceiveDoc "hello"
+  ]}
+foldp (ReceiveDoc d) s =
+  { state: s
+  , effects: []
   }
 
-main :: ∀ fx. Eff (channel :: CHANNEL, exception :: EXCEPTION, console :: CONSOLE, dom :: DOM | fx) Unit
+main :: ∀ fx. Eff (channel :: CHANNEL, exception :: EXCEPTION, console :: CONSOLE, dom :: DOM, ajax :: AJAX | fx) Unit
 main = do
   app <- start
-    { initialState: { form: "" }
+    { initialState: { form: defaultUrl }
     , view
     , foldp
     , inputs: []
--- livedoc-ui-purescript/src/State.purs
+++ livedoc-ui-purescript/src/State.purs
@@ -2,9 +2,14 @@ module State where
 
 import Pux.DOM.Events (DOMEvent)
 
-type State = {
-  form :: String
+type State =
+  { form :: String
   }
 
-data Event = FormUpdate String
+type Url = String
+
+data Event
+  = FormUpdate String
   | FormSubmit DOMEvent
+  | RequestDoc Url
+  | ReceiveDoc String
